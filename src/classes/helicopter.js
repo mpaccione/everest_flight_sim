b7f5f02ca1a1f54168b0326ae291b4cd2c43c11e
Michael Paccione <mpaccione1991@gmail.com>
2019-05-31 11:03:34 -0700

    Physics WIP

--- src/classes/helicopter.js
+++ src/classes/helicopter.js
@@ -226,12 +226,12 @@ class Helicopter {
 			}
 		} else if ( this.roll != 0 ) {
 			// Calc Roll Vector with Trigonometry
-			this.vX = Math.abs(vYOriginal * Math.cos(rollRads));
-			this.vY = Math.abs(vYOriginal * Math.sin(rollRads));
+			this.vX = vYOriginal * Math.cos(rollRads);
+			this.vY = vYOriginal * Math.sin(rollRads);
 		} else if ( this.pitch != 0 ) {
 			// Calc Pitch Vector with Trigonometry
-			this.vY = Math.abs(vYOriginal * Math.sin(pitchRads));
-			this.vZ = Math.abs(vYOriginal * Math.cos(pitchRads));
+			this.vY = vYOriginal * Math.sin(pitchRads);
+			this.vZ = vYOriginal * Math.cos(pitchRads);
 		}
 	}
 
@@ -251,7 +251,7 @@ class Helicopter {
 		// Invert Velocity Based on Roll Value
 		this.heli.position.x += this.roll > 0 ? -this.vX*multiplier : this.vX*multiplier;
 		// Invert Velocity Based on Pitch Value
-		this.heli.position.z += this.pitch > 0 ? this.vZ*multiplier : -this.vZ*multiplier;
+		this.heli.position.z += this.vZ*multiplier;
 		// Need to add code to fix falling so it is relative to the ground and not the vectors of the helicopter
 
 		this.x = this.heli.position.x;
