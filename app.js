bf6a5fe860ddbb4dc26dbd5c0ddbac2cdd70a837
Michael Paccione <mpaccione1991@gmail.com>
2019-12-05 10:47:52 -0800

    Terrain WIP + Object Picking

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const THREE = require('three');
 const GLTFLoader = require('three-gltf-loader');
 const Helicopter = require('./src/classes/helicopter');
 const OrbitControls = require('three-orbit-controls')(THREE);
+const terrainData = require('./Grid_Output_Everest_10.json');
 
 // View
 const scene = new THREE.Scene(),
@@ -10,50 +11,164 @@ const scene = new THREE.Scene(),
 	  renderer = new THREE.WebGLRenderer(),
 	  controls = new OrbitControls(camera);
 
+let time;
+
 controls.keys = false;
 
-// Group
-const miniHeliGroup = new THREE.Group();
+// Grid for Reference
+// Axis Helper X: Red, Y: Green, Z: Blue
+const axisHelper = new THREE.AxisHelper(8000),
+	  gridSize = 8000,
+	  gridDivisions = 10;
+	  gridArr =  [];
 
-// Load Helicopter Model
-const miniModelLoader = new GLTFLoader();
+scene.add(axisHelper);
 
-miniModelLoader.load( './src/models/helicopter/scene.gltf', function(gltf){
-	miniModel = gltf.scene;
-	miniModel.name = "miniHeli"
-	miniModel.rotation.y = -90 * Math.PI / 180; // Radians
-    miniModel.position.set( 0, 0, 0 );
+console.log(terrainData);
 
-    let miniModelMesh = miniModel.children[0].children[0].children[0],
-    	miniModelMeshArr = [ miniModelMesh.children[0], miniModelMesh.children[1], miniModelMesh.children[2] ];
+for (var i = 0; i < 4; i++) {
+	const grid = new THREE.GridHelper(gridSize, gridDivisions);
+	grid.position.set(0, 0, (gridSize * -i))
+	gridArr.push(grid);
+}
 
-    console.log(miniModelMeshArr);
+for (var i = 0; i < gridArr.length; i++) {
+	scene.add(gridArr[i]);
+}
 
-    for (var i = miniModelMeshArr.length - 1; i >= 0; i--) {
-    	miniModelMeshArr[i].name = "mesh"+i;		
-    	miniModelMeshArr[i].material.wireframe = true;
+for (var j = 0; j < 4; j++) {
+	const gridTile = terrainData[j];
+	for (var k = 0; k < gridTile.length; k++) {
+		const subGridTile = gridTile[k];
+		// Row
+		for (var a = 0; a < subGridTile.length; a++) {
+			// Column
+			for (var b = 0; b < subGridTile[a].length; b++) {
+				const elevation = subGridTile[a][b]["elevation"],
+					  geometry = new THREE.BoxGeometry( 800, elevation, 800 ),
+					  material = new THREE.MeshBasicMaterial( {wireframe: false, color: colorData(elevation)} ),
+					  cube = new THREE.Mesh( geometry, material );
+
+				console.log(elevation);
+				console.log(`${0}, ${a*800}, ${b*800}`);
+
+				cube.position.set( (a*800)-4000, 2000, ((b*800)-4000)+(j*-4000) )
+				scene.add( cube );
+			}
+		}
+		// lat -> Z axis
+		// long -> X axis
+	}
+}
+
+console.log(scene);
+
+function colorData(meters) {
+	let rgbString = "",
+	    r         = parseInt((meters/8848) * 255),
+	    g         = 255 - r;
+
+	r = r < 0 ? 0 : r;
+	rgbString = `rgb(${r},${g}, 0)`;
+	return rgbString;
+}
+
+function resizeRendererToDisplaySize(renderer) {
+	const canvas = renderer.domElement,
+		  width = canvas.clientWidth,
+		  height = canvas.clientHeight,
+		  needResize = canvas.width !== width || canvas.height !== height;
+
+	if (needResize) {
+	  renderer.setSize(width, height, false);
+	}
+
+	return needResize;
+}
+
+class PickHelper {
+    constructor() {
+      this.raycaster = new THREE.Raycaster();
+      this.pickedObject = null;
+      this.pickedObjectSavedColor = 0;
     }
 
-    miniHeliGroup.add( new THREE.AxesHelper(500) )
-	miniHeliGroup.add( miniModel );
-	scene.add(miniHeliGroup);
-} )
+    pick(normalizedPosition, scene, camera, time) {
+      // restore the color if there is a picked object
+      if (this.pickedObject) {
+        // this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
+        this.pickedObject.material.color.setHex(this.pickedObjectSavedColor);
+        this.pickedObject = undefined;
+      }
 
-// Initiate Helicopter
-const player = new Helicopter(miniHeliGroup, "Wireframe", 14000);
+      // cast a ray through the frustum
+      this.raycaster.setFromCamera(normalizedPosition, camera);
+      // get the list of objects the ray intersected
+      const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+      if (intersectedObjects.length) {
+        // pick the first object. It's the closest one
+        this.pickedObject = intersectedObjects[0].object;
+        // save its color
+        // this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+        this.pickedObjectSavedColor = this.pickedObject.material.color.getHex();
+        // set its emissive color to flashing red/yellow
+        // this.pickedObject.material.emissive.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
+        this.pickedObject.material.color.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
+      }
+    }
+}
 
-// Grid for Reference
-const gridSize = 10000,
-	  gridDivisions = 40,
-	  gridHelper = new THREE.GridHelper(gridSize, gridDivisions);
+function getCanvasRelativePosition(event) {
+	// const rect = document.querySelectorAll('canvas')[0].getBoundingClientRect();
 
-scene.add(gridHelper);
+	return {
+	  x: event.clientX - 0,
+	  y: event.clientY - 0,
+	};
+}
+
+function setPickPosition(event) {
+	const pos = getCanvasRelativePosition(event);
+	pickPosition.x = (pos.x / window.innerWidth ) *  2 - 1;
+	pickPosition.y = (pos.y / window.innerHeight) * -2 + 1;  // note we flip Y
+}
+
+function clearPickPosition() {
+	// unlike the mouse which always has a position
+	// if the user stops touching the screen we want
+	// to stop picking. For now we just pick a value
+	// unlikely to pick something
+	pickPosition.x = -100000;
+	pickPosition.y = -100000;
+}
+
+
+const pickPosition = {x: 0, y: 0};
+const pickHelper = new PickHelper();
+clearPickPosition();
+
+// Mouse Picker Listeners
+window.addEventListener('mousemove', setPickPosition);
+window.addEventListener('mouseout', clearPickPosition);
+window.addEventListener('mouseleave', clearPickPosition);
+
+window.addEventListener('touchstart', (event) => {
+	// prevent the window from scrolling
+	event.preventDefault();
+	setPickPosition(event.touches[0]);
+}, {passive: false});
+
+window.addEventListener('touchmove', (event) => {
+	setPickPosition(event.touches[0]);
+});
+
+window.addEventListener('touchend', clearPickPosition);
 
 // Camera
 camera.name = "camera";
 camera.position.z = 5000;
 camera.position.y = 5000;
-camera.lookAt(miniHeliGroup.position);
+// camera.lookAt(miniHeliGroup.position);
 
 // Debugging
 window.scene = scene;
@@ -66,8 +181,16 @@ document.body.appendChild( renderer.domElement );
 
 // Animation Loop
 const animate = function () {
+	if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    time *= 0.001;
+    pickHelper.pick(pickPosition, scene, camera, time);
+
 	requestAnimationFrame( animate );
-	player.update()
 	renderer.render( scene, camera );
 };
 
