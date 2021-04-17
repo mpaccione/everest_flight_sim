4048c9b42c6bad805eaf5ad092adc3973c73e435
mpaccione <mpaccione1991@gmail.com>
2021-04-16 23:50:42 -0700

    Download Tileset

--- server/getRGBDEM.mjs
+++ server/getRGBDEM.mjs
@@ -10,35 +10,89 @@ const queryMapbox = async () => {
   const zoom = 14;
   const long = 86.922623;
   const lat = 27.986065;
+  const topLeft = { lat: 28.08416, long: 86.622876 }; // Top Left Corner
+  const bottomRight = { lat: 27.680334, long: 87.230674 }; // Bottom Right Corner
   // Convert for Mapbox
-  const xyzTilebelt = tilebelt.pointToTile(long, lat, zoom);
+  const xyzTile = tilebelt.pointToTile(long, lat, zoom);
+  const bboxTile = tilebelt.bboxToTile([
+    topLeft.long,
+    topLeft.lat,
+    bottomRight.long,
+    bottomRight.lat,
+  ]);
+  // Get Higher Zoom Level
 
-  try {
-    // Example from Docs - Omaha, Nebraska
-    // const response = await fetch(
-    //   `https://api.mapbox.com/v4/mapbox.terrain-rgb/14/12558/6127.pngraw?access_token=${process.env.ACCESS_TOKEN}`
-    // );
-    // const filename = "omaha-rgb.png"
+  /////////////////////////////////////
+  // Downloads Tileset Covering BBOX //
+  /////////////////////////////////////
+  downloadTileset(bboxTile, "everest-rgb");
+  async function downloadTileset(bboxTile, filename) {
+    const tilesetArr = tilebelt.getChildren(bboxTile);
+    let count = 0;
 
-    const response = await fetch(
-      `https://api.mapbox.com/v4/mapbox.terrain-rgb/${zoom}/${xyzTilebelt[0]}/${xyzTilebelt[1]}.pngraw?access_token=${process.env.ACCESS_TOKEN}`
-    );
-    const filename = "everest-rgb.png";
+    (async function recursiveAJAX(tile, index) {
+      try {
+        const response = await fetch(
+          `https://api.mapbox.com/v4/mapbox.terrain-rgb/${tile[2]}/${tile[0]}/${tile[1]}@2x.pngraw?access_token=${process.env.ACCESS_TOKEN}`
+        );
 
-    await new Promise((resolve, reject) => {
-      const fileStream = fs.createWriteStream(`./data/${filename}`);
-      response.body.pipe(fileStream);
-      response.body.on("error", (err) => {
-        reject(err);
+        await new Promise((resolve, reject) => {
+          const fileStream = fs.createWriteStream(
+            `./data/${filename}-${index}.png`
+          );
+          response.body.pipe(fileStream);
+          response.body.on("error", (err) => {
+            reject(err);
+          });
+          fileStream.on("finish", function () {
+            resolve();
+            // Iterate until all tiles are complete
+            if (tilesetArr.length > 0) {
+              count++;
+              recursiveAJAX(tilesetArr.shift(), count);
+            }
+          });
+        });
+
+        console.log({ response });
+      } catch (err) {
+        console.error(err);
+      }
+    })(tilesetArr.shift(), 0);
+  }
+
+  /////////////////////////////////////////
+  // Downloads Single Tile Covering BBOX //
+  /////////////////////////////////////////
+
+  // downloadSingleTile(bboxTile, "everest-rgb")
+  async function downloadSingleTile(bboxTile, filename) {
+    try {
+      // Example from Docs - Omaha, Nebraska
+      // const response = await fetch(
+      //   `https://api.mapbox.com/v4/mapbox.terrain-rgb/14/12558/6127.pngraw?access_token=${process.env.ACCESS_TOKEN}`
+      // );
+      // const filename = "omaha-rgb.png"
+
+      const response = await fetch(
+        `https://api.mapbox.com/v4/mapbox.terrain-rgb/${bboxTile[2]}/${bboxTile[0]}/${bboxTile[1]}.pngraw?access_token=${process.env.ACCESS_TOKEN}`
+      );
+
+      await new Promise((resolve, reject) => {
+        const fileStream = fs.createWriteStream(`./data/${filename}.png`);
+        response.body.pipe(fileStream);
+        response.body.on("error", (err) => {
+          reject(err);
+        });
+        fileStream.on("finish", function () {
+          resolve();
+        });
       });
-      fileStream.on("finish", function () {
-        resolve();
-      });
-    });
 
-    console.log({ response });
-  } catch (err) {
-    console.error(err);
+      console.log({ response });
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
 
