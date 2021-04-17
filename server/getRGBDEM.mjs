3f33eefdb497fb4b9dabce5975d30f177382a99e
mpaccione <mpaccione1991@gmail.com>
2021-04-16 22:52:24 -0700

    Received Everest Tile

--- server/getRGBDEM.mjs
+++ server/getRGBDEM.mjs
@@ -1,33 +1,32 @@
 import fs from "fs";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-import SphericalMercator from "@mapbox/sphericalmercator";
+import tilebelt from "@mapbox/tilebelt";
 
 dotenv.config();
 
 const queryMapbox = async () => {
-  const merc = new SphericalMercator({
-    size: 256,
-  });
-  const xyFromLatLong = (lat, long, merc) => {
-    return merc.forward([long, lat]);
-  };
-
-  const zoom = 9;
+  // Coords
+  const zoom = 14;
   const long = 86.922623;
   const lat = 27.986065;
-  const xyPos = xyFromLatLong(lat, long, merc);
-
-  console.log({ xyPos });
+  // Convert for Mapbox
+  const xyzTilebelt = tilebelt.pointToTile(long, lat, zoom);
 
   try {
     // Example from Docs - Omaha, Nebraska
+    // const response = await fetch(
+    //   `https://api.mapbox.com/v4/mapbox.terrain-rgb/14/12558/6127.pngraw?access_token=${process.env.ACCESS_TOKEN}`
+    // );
+    // const filename = "omaha-rgb.png"
+
     const response = await fetch(
-      `https://api.mapbox.com/v4/mapbox.terrain-rgb/14/12558/6127.pngraw?access_token=${process.env.ACCESS_TOKEN}`
+      `https://api.mapbox.com/v4/mapbox.terrain-rgb/${zoom}/${xyzTilebelt[0]}/${xyzTilebelt[1]}.pngraw?access_token=${process.env.ACCESS_TOKEN}`
     );
+    const filename = "everest-rgb.png";
 
     await new Promise((resolve, reject) => {
-      const fileStream = fs.createWriteStream("./data/omaha-rgb.png");
+      const fileStream = fs.createWriteStream(`./data/${filename}`);
       response.body.pipe(fileStream);
       response.body.on("error", (err) => {
         reject(err);
@@ -36,12 +35,8 @@ const queryMapbox = async () => {
         resolve();
       });
     });
-    // const response = await fetch(
-    //   `https://api.mapbox.com/v4/mapbox.terrain-rgb/${zoom}/${xyPos[0]}/${xyPos[1]}.pngraw?access_token=${process.env.ACCESS_TOKEN}`
-    // );
 
     console.log({ response });
-    // console.log({ res });
   } catch (err) {
     console.error(err);
   }
